f815009dcc3e140288009fa034785a075f4da170
harikrishnan <kphari891@gmail.com>
2022-11-21 13:32:08 +0530

    Initial commit

---
+++ bin/project_2.dart
@@ -0,0 +1,15 @@
+void main () {
+  String name = "Devi";
+  int age = 22;
+  double mark = 8.3;
+  String email = 'devicathi91@gmail.com';
+  int phone = 7045673567;
+
+  print('My details'); //direct string printing
+  print("my name is $name"); //string interpolation syntax -> ${}
+  print('i am $age yrs old');
+  print('mark : $mark');
+  print('$email is my email id');
+  print('Phone $phone');
+
+}
